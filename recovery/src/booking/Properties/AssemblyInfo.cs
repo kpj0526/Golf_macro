a3160b7f30ff8400a94541d8b85053eff08cacac
kpj0526 <kpj080526@gmail.com>
2026-09-14 10:25:23 +0900

    Release v0.7.0

--- recovery/src/booking/Properties/AssemblyInfo.cs
+++ recovery/src/booking/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.Versioning;
 
 [assembly: AssemblyCompany("booking")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
-[assembly: AssemblyInformationalVersion("0.6.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyInformationalVersion("0.7.0")]
 [assembly: AssemblyProduct("booking")]
 [assembly: AssemblyTitle("booking")]
 [assembly: TargetPlatform("Windows7.0")]
 [assembly: SupportedOSPlatform("Windows7.0")]
-[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
